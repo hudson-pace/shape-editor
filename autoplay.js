1b57af72e20c39a7f2b781e8afea5ee5f87eee3e
Hudson Pace <hudsonmpace@gmail.com>
2024-01-05 13:17:43 -0700

    Make solver simpler/more readable.

--- autoplay.js
+++ autoplay.js
@@ -1,121 +1,123 @@
 import gameUtils from './gameUtils.js';
 
-const getClickTargets = (tiles) => {
-  const clickTargets = new Set();
-  tiles.forEach((tile) => {
-      if (tile.exposed && tile.value === gameUtils.countNeighboringFlags(tile)) {
-        tile.neighbors.forEach((n) => {
-          if (!n.exposed && !n.flagged) {
-            clickTargets.add(n);
-          }
+
+const getFlagCountRequirement = (tiles, flagCount) => {
+    return {
+        tiles: tiles.filter((t) => !t.flagged && !t.exposed), // All unflagged and unexposed tiles
+        count: flagCount // The number of remaining flags
+    }
+}
+
+const autoplay = (tiles, getFlagCount) => {
+    const constraints = gameUtils.getFrontier(tiles).map((tile) => {
+        return {
+            tiles: gameUtils.getUnexposedUnflaggedNeighbors(tile),
+            count: tile.value - gameUtils.countNeighboringFlags(tile),
+        }
+    });
+
+    const clickTargets = new Set();
+    const flagTargets = new Set();
+
+    constraints.forEach((constraint) => {
+        const relevantConstraints = findRelevantConstraints(constraints, constraint);
+        const viableStrings = [];
+        generateBinaryStrings(constraint.tiles.length, constraint.count).forEach((s) => {
+            const mines = [];
+            const safe = [];
+            for (let i = 0; i < s.length; i++) {
+                if (s.charAt(i) === '1') {
+                    mines.push(constraint.tiles[i]);
+                } else {
+                    safe.push(constraint.tiles[i]);
+                }
+            }
+            let viable = true;
+            for (let i = 0; i < relevantConstraints.length; i++) {
+                if (!fitsConstraint(relevantConstraints[i], mines, safe)) {
+                    viable = false;
+                    break;
+                }
+            }
+            if (viable) {
+                viableStrings.push(s);
+            }
         });
-      }
-  });
-  return clickTargets;
-}
 
-// Accepts an array of arrays. Returns common elements.
-const getCommonElements = (arrays) => {
-  let common = arrays[0];
-  for (let i = 1; i < arrays.length; i++) {
-      common = common.filter((el) => arrays[i].includes(el));
-  }
-  return common;
-}
+        if (viableStrings.length !== 0) {
+            for (let i = 0; i < constraint.tiles.length; i++) {
+                let allMine = true;
+                let allSafe = true;
+                viableStrings.forEach((s) => {
+                    if (s.charAt(i) === '1') {
+                        allSafe = false;
+                    } else if (s.charAt(i) === '0') {
+                        allMine = false;
+                    }
+                });
+                if (allMine) {
+                    flagTargets.add(constraint.tiles[i]);
+                } else if (allSafe) {
+                    clickTargets.add(constraint.tiles[i]);
+                }
+            }
+        }
+    });
 
-const getSubsets = (arr, arrays) => {
-  return arrays.filter((a) => a.tiles.every((el) => arr.includes(el)));
-}
-
-const autoplay = (tiles) => {
-  const clickTargets = getClickTargets(tiles);
-  const flagTargets = new Set();
-
-  const requirements = gameUtils.getFrontier(tiles).map((tile) => {
-      return {
-          tiles: gameUtils.getUnexposedUnflaggedNeighbors(tile),
-          count: tile.value - gameUtils.countNeighboringFlags(tile),
-      }
-  });
-
-  requirements.forEach((req) => {
-      if (req.count === req.tiles.length) {
-          flagTargets.add(...req.tiles);
-      } else {
-          const subsets = getSubsets(req.tiles, requirements);
-          let frontier = [...subsets]
-          while (frontier.length > 0) {
-              const oldFrontier = [...frontier];
-              const newFrontier = [];
-              oldFrontier.forEach((s) => {
-                  const complement = [...oldFrontier, ...subsets].find((s2) => getCommonElements([s.tiles, s2.tiles]).length === 0);
-                  if (complement) {
-                      const newSubset = {
-                          tiles: [...s.tiles, ...complement.tiles],
-                          count: s.count + complement.count
-                      };
-                      subsets.push(newSubset);
-                      newFrontier.push(newSubset);
-                  }
-              });
-              frontier = newFrontier;
-          }
-          const relevantSubsets = subsets.filter((s) => s.count === req.count);
-          relevantSubsets.forEach((s) => {
-              const what = req.tiles.filter((t) => !s.tiles.includes(t));
-              if (what.length > 0) {
-                  clickTargets.add(...what);
-              }
-          });
-      }
-  });
-
-  requirements.forEach((req) => {
-      const relevantReqs = requirements.filter((r) => getCommonElements([req.tiles, r.tiles]).length > 0);
-      const combinationStrings = generateBinaryStrings(req.tiles.length, req.count);
-      const combinations = [];
-      combinationStrings.forEach((s) => {
-          const combination = [];
-          for (let i = 0; i < s.length; i++) {
-              if (s.charAt(i) === '1') {
-                  combination.push(req.tiles[i]);
-              }
-          }
-          combinations.push(combination);
-      });
-
-      const validCombinations = combinations.filter((c) => {
-          for (let i = 0; i < relevantReqs.length; i++) {
-              if (getCommonElements([c, relevantReqs[i].tiles]).length > relevantReqs[i].count) {
-                  return false;
-              }
-          }
-          return true;
-      });
-      const commonElements = getCommonElements(validCombinations);
-      if (commonElements.length > 0) {
-          flagTargets.add(...commonElements);
-      }
-  });
-  return {
-      clickTargets,
-      flagTargets,
-  }
+    return {
+        clickTargets,
+        flagTargets,
+    }
 }
 export default autoplay;
 
+// Given a list of constraints and a specific constraint, return a list of all relevant constraints.
+// Specifically, all constraints which contain at least one tile contained in the given constraint.
+// ie, if constraint deals with tiles (a,b,c), return constraints dealing with (a,b), (b), (c,d,e), etc.
+const findRelevantConstraints = (constraints, constraint) => {
+    const relevantConstraints = [];
+    constraints.forEach((c) => {
+        if (c.tiles.find((t) => constraint.tiles.find((tile) => t === tile))) {
+            relevantConstraints.push(c);
+        }
+    });
+    return relevantConstraints;
+}
+
+// Provides a constraint, and lists of tiles that are mines and safe. Tests if those lists fit the constraint.
+// Returns false if that combination of tiles would necessarily mean too many or too few mines.
+const fitsConstraint = (constraint, mines, safe) => {
+    let mineCount = 0;
+    let safeCount = 0;
+    let undecidedCount = 0;
+    constraint.tiles.forEach((tile) => {
+        if (mines.includes(tile)) {
+            mineCount++;
+        } else if (safe.includes(tile)) {
+            safeCount++;
+        } else {
+            undecidedCount++;
+        }
+    });
+    // Undecided tiles can go either way. If all undecided are mines and the count is too low,
+    // or if all undecided are safe and the count is too high, the constraint is violated.
+    if (undecidedCount + mineCount < constraint.count || mineCount > constraint.count) {
+        return false;
+    }
+    return true;
+}
 
 
 
 const generateBinaryStrings = (length, oneCount) => {
-const total = Math.pow(2, length);
-const strings = [];
-for (let i = 0; i < total; i++) {
-  const str = i.toString(2);
-  if (str.split('1').length === oneCount + 1) {
-    const pad = length - str.length;
-    strings.push('0'.repeat(pad).concat(str));
-  }
-}
-return strings;
+    const total = Math.pow(2, length);
+    const strings = [];
+    for (let i = 0; i < total; i++) {
+        const str = i.toString(2);
+        if (str.split('1').length === oneCount + 1) {
+            const pad = length - str.length;
+            strings.push('0'.repeat(pad).concat(str));
+        }
+    }
+    return strings;
 }
